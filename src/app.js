f7cd2990a84a315e78795d04c2d59aecc7a8f18e
Adrian <142854436+withyso@users.noreply.github.com>
2023-10-22 21:29:52 +0000

    Boton y timer añadidos

--- src/app.js
+++ src/app.js
@@ -4,61 +4,72 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-  const suits = ["1", "2", "3", "4"];
-  const number = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
+  setInterval(() => {}, 1000 * 10);
+
   //Selectores de Html
   let htmlTopSuit = document.querySelector("#top-suit");
   let htmlBotSuit = document.querySelector("#bot-suit");
   let htmlNumber = document.querySelector(".number");
+  const htmlButton = document.querySelector("button");
+
   //Funciones
-  function randomNumber(array, htmlElement) {
-    let iteration = Math.floor(Math.random() * array.length);
-    htmlElement.innerHTML = array[iteration];
+  randomNumber(htmlNumber);
+  randomSuit(htmlTopSuit, htmlBotSuit);
+
+  htmlButton.addEventListener("click", event => {
+    randomNumber(htmlNumber);
+    randomSuit(htmlTopSuit, htmlBotSuit);
+  });
+
+  function randomNumber(htmlElement) {
+    const number = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let iteration = Math.floor(Math.random() * number.length);
+    htmlElement.innerHTML = number[iteration];
+    return htmlElement;
   }
 
-  function randomSuit(array, htmlElement, htmlElement2) {
-    let iteration = Math.floor(Math.random() * array.length);
-    if (iteration === 1) {
+  function randomSuit(htmlElement, htmlElement2) {
+    const suits = ["1", "2", "3", "4"];
+    let iteration = Math.floor(Math.random() * suits.length);
+    if (iteration == 1) {
       htmlElement.innerHTML = "&clubs;";
+      htmlElement.classList.add("text-primary");
       htmlElement2.innerHTML = "&clubs;";
-      htmlElement.classList.add("text-success");
-      htmlElement2.classList.add("text-success");
+      htmlElement2.classList.add("text-primary");
     }
-    if (iteration === 2) {
+    if (iteration == 2) {
       htmlElement.innerHTML = "&diams;";
-      htmlElement.classList.add("text-warning");
+      htmlElement.classList.add("text-danger");
       htmlElement2.innerHTML = "&diams;";
-      htmlElement2.classList.add("text-warning");
+      htmlElement2.classList.add("text-danger");
     }
-    if (iteration === 3) {
+    if (iteration == 3) {
       htmlElement.innerHTML = "&hearts;";
       htmlElement.classList.add("text-danger");
       htmlElement2.innerHTML = "&hearts;";
       htmlElement2.classList.add("text-danger");
     }
-    if (iteration === 4) {
+    if (iteration == 4) {
       htmlElement.innerHTML = "&spades;";
       htmlElement2.innerHTML = "&spades;";
     }
+    return htmlElement, htmlElement2;
   }
-  randomNumber(number, htmlNumber);
-  randomSuit(suits, htmlTopSuit, htmlBotSuit);
 };
